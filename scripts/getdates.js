95627c0be1f5ec3f8470da02a254f58d152438a2
Alebproject32 <isaacale25@gmail.com>
2024-11-20 21:30:59 -0400

    Last modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Set the current year
-    const currentYearSpan = document.getElementById('currentyear');
+    const currentYearSpan = document.getElementById('currentYear');
     const currentYear = new Date().getFullYear();
     currentYearSpan.textContent = currentYear;
 
     // Set the last modified date
-    let lastModified = document.lastModified;
-    console.log("lastModified: " + lastModified);});
+    const lastModifiedElement = document.getElementaryById('lastModified');
+    lastModifiedElement.textContent = "Last Modified: " + document.lastModified;});
