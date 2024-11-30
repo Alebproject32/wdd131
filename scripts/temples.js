33ebfe3e2c4b0e5f674d58a863b72ed7e03d43cc
Alebproject32 <isaacale25@gmail.com>
2024-11-30 09:13:37 -0400

    I fixed Javascript code, avoding repeat code from CSS, and adding some detals in HTML.

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Set the current year
-    const currentYearSpan = document.getElementById('currentYear');
-    const currentYear = new Date().getFullYear();
-    currentYearSpan.textContent = currentYear;
+  // Set the current year
+  const currentYearSpan = document.getElementById('currentYear');
+  const currentYear = new Date().getFullYear();
+  currentYearSpan.textContent = currentYear;
 
-    // Set the last modified date
-    const lastModifiedElement = document.getElementaryById('lastModified');
-    lastModifiedElement.textContent = "Last Modified: " + document.lastModified;});
+  // Set the last modified date
+  const lastModifiedElement = document.getElementById('lastModified');
+  lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const menuIcon = document.getElementById(".menu-icon");
-        const closeIcon = document.getElementById(".close-icon");
-        const navUl = document.getElementById("nav-list");
-      
-        menuIcon.addEventListener("click", () => {
-          navUl.classList.toggle("active");
-          menuIcon.classList.add("hidden");
-          closeIcon.style.display = "block";
-        });
-      
-        closeIcon.addEventListener("click", () => {
-          navUl.classList.remove("active");
-          menuIcon.classList.remove("hidden");
-          closeIcon.style.display = "none";
-        });
-      });
+  // Menu toggle functionality
+  const menuIcon = document.querySelector(".menu-icon");
+  const closeIcon = document.querySelector(".close-icon");
+  const navUl = document.getElementById("nav-list");
+
+  menuIcon.addEventListener("click", () => {
+      navUl.classList.toggle("active");
+      menuIcon.classList.add("hidden");
+      closeIcon.style.display = "block";
+  });
+
+  closeIcon.addEventListener("click", () => {
+      navUl.classList.remove("active");
+      menuIcon.classList.remove("hidden");
+      closeIcon.style.display = "none";
+  });
+});
+
       
