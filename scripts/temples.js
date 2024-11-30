5c8f6a57e6207fef0caab1fdc65f97fbbbfce995
Alebproject32 <isaacale25@gmail.com>
2024-11-30 08:06:24 -0400

    I added menu-icon and close-icon as well as Javascript function

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set the last modified date
     const lastModifiedElement = document.getElementaryById('lastModified');
     lastModifiedElement.textContent = "Last Modified: " + document.lastModified;});
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuIcon = document.getElementById(".menu-icon");
+        const closeIcon = document.getElementById(".close-icon");
+        const navUl = document.getElementById("nav-list");
+      
+        menuIcon.addEventListener("click", () => {
+          navUl.classList.toggle("active");
+          menuIcon.classList.add("hidden");
+          closeIcon.style.display = "block";
+        });
+      
+        closeIcon.addEventListener("click", () => {
+          navUl.classList.remove("active");
+          menuIcon.classList.remove("hidden");
+          closeIcon.style.display = "none";
+        });
+      });
+      
