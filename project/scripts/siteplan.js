c2c50617c2dd1c583b80a03b40915bb0fa80f408
Alebproject32 <isaacale25@gmail.com>
2024-12-17 21:06:31 -0400

    Fixing some error in Javascript

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -9,19 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
     lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
   
     // Menu toggle functionality
-    const menuIcon = document.querySelector("menu-icon");
-    const closeIcon = document.querySelector("close-icon");
+    const menuicon = document.querySelector(".menu-icon");
+    const closeicon = document.querySelector(".close-icon");
     const navUl = document.getElementById("nav-list");
   
-    menuIcon.addEventListener("click", () => {
+    menuicon.addEventListener("click", () => {
         navUl.classList.toggle("active");
-        menuIcon.classList.add("hidden");
-        closeIcon.style.display = "block";
+        menuicon.classList.add("hidden");
+        closeicon.style.display = "block";
     });
   
-    closeIcon.addEventListener("click", () => {
+    closeicon.addEventListener("click", () => {
         navUl.classList.remove("active");
-        menuIcon.classList.remove("hidden");
-        closeIcon.style.display = "none";
+        menuicon.classList.remove("hidden");
+        closeicon.style.display = "none";
     });
   });
