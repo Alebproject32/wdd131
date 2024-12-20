9f9a302fa500cf5e684ff5d1b8cd53f8b290576a
Alebproject32 <isaacale25@gmail.com>
2024-12-19 20:10:38 -0400

    Added subscribe html, css and Javascript to my Project

---
+++ scripts/subscribe.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Set the current year
+    const currentYearSpan = document.getElementById('currentYear');
+    const currentYear = new Date().getFullYear();
+    currentYearSpan.textContent = currentYear;
+  
+    // Set the last modified date
+    const lastModifiedElement = document.getElementById('lastModified');
+    lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
+    });
